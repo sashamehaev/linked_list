a7a6dc1022382aea93806193751cbd785155bb67
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2024-04-29 14:34:50 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# Импорт модуля os из стандартной библиотеки 
+# для взаимодействия с операционной системой.
+import os
+# Считывание переменной окружения REMOTE_JUDGE, чтобы определить, 
+# локально выполняется код или на удалённом сервере.
+LOCAL = os.environ.get('REMOTE_JUDGE', 'false') != 'true'
+
+# Если код выполняется на локальном компьютере, то...
+if LOCAL:
+    # Класс, описывающий элементы связного списка:
+    class Node:
+        def __init__(self, value, next_item=None):
+            self.value = value
+            self.next_item = next_item
+
+
+def solution(head_node, idx):
+    # Напишите код функции здесь.
+    # ヽ(´▽`)/
+    index = 0
+    temp_node = head_node
+    while temp_node is not None:
+        if idx == 0:
+            return head_node.next_item
+        if index == idx:
+            if temp_node.next_item is None:
+                head_node.next_item.next_item.next_item = None
+                return head_node
+            if temp_node.next_item.next_item is None:
+                head_node.next_item.next_item = temp_node.next_item
+                return head_node
+            if temp_node.next_item.next_item.next_item is None:
+                head_node.next_item = temp_node.next_item
+                return head_node
+        temp_node = temp_node.next_item
+        index += 1
+    # return head_node
+
+
+# Тестирующая функция для проверки решения.
+# Не изменяйте её, она не требует вашего внимания.
+def test():
+    node3 = Node("Задача 4: Обследовать грунт в радиусе 3 м", None)
+    node2 = Node("Задача 3: Измерить температуру атмосферы", node3)
+    node1 = Node("Задача 2: Пробурить скважину глубиной 0.5 м", node2)
+    node0 = Node("Задача 1: Фотосъёмка 360°", node1)
+
+    new_head = solution(node0, 1)
+    # Выражение, начинающееся с ключевого слова assert,
+    # проверяет, что утверждение в выражении истинно.
+    # Если утверждение ложно - в этом месте возникнет ошибка.
+    assert new_head is node0
+    assert new_head.next_item is node2
+    assert new_head.next_item.next_item is node3
+    assert new_head.next_item.next_item.next_item is None
+    # result is node0 -> node2 -> node3
+
+
+if __name__ == '__main__':
+    test()
